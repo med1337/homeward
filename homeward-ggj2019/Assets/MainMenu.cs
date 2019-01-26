e337f7557a63da475f24e9f946a8f1b52ed216e0
Charlie <Charlie2.saunders@live.uwe.ac.uk>
2019-01-26 22:23:25 +0000

    Added TextMeshPro and Fixed Menu

---
+++ homeward-ggj2019/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+    public void ToggleMusic()
+    {
+        //transform.Find("MUSIC OFF").GetComponent<TextMeshPro>()
+    }
+    public void ToggleFX()
+    {
+
+    }
+}
