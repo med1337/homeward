250a1e37e9744510427ac5c6c0893ed76635349a
Elliott Smith <elliott2.smith@live.uwe.ac.uk>
2019-01-27 03:01:40 +0000

    leaves when flying through trees

---
+++ homeward-ggj2019/Assets/Scripts/TreeLeaves.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeLeaves : MonoBehaviour
+{
+    [SerializeField] ParticleSystem leaves;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<FlockMember>())
+        {
+            ParticleSystem leaf = Instantiate(leaves, other.transform.position, transform.rotation);
+
+            Destroy(leaf.gameObject, 6);
+        }
+    }
+}
