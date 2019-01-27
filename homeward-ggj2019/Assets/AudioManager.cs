8975015115778d9519d4fd73b53b5fa4fa3dadc4
Charlie <Charlie2.saunders@live.uwe.ac.uk>
2019-01-27 00:01:50 +0000

    Added Audio Manager and Bird dying SFX

---
+++ homeward-ggj2019/Assets/AudioManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
+    public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
+    public static AudioManager instance = null;     //Allows other scripts to call functions from SoundManager.             
+    public float lowPitchRange = .95f;              //The lowest a sound effect will be randomly pitched.
+    public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
+    [SerializeField] List<AudioClip> audioClips;
+
+    void Awake()
+    {
+        //Check if there is already an instance of SoundManager
+        if (instance == null)
+            //if not, set it to this.
+            instance = this;
+        //If instance already exists:
+        else if (instance != this)
+            //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+            Destroy(gameObject);
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    //Used to play single sound clips.
+    public void PlaySingle(AudioClip clip)
+    {
+        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
+        efxSource.clip = clip;
+
+        //Play the clip.
+        efxSource.PlayOneShot(clip);
+    }
+
+    public void PlayLooped(AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.Play();
+        efxSource.loop = true;
+    }
+
+    public void PlaySingleAtSource(AudioSource src)
+    {
+        //Play the clip.
+        src.PlayOneShot(src.clip);
+    }
+
+    public void PlaySingleAtSource(AudioSource src, string clip)
+    {
+        var ac = GetAudioClip(clip);
+        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
+        src.clip = ac;
+        //Play the clip.
+        src.PlayOneShot(src.clip);
+    }
+
+    public void PlaySingleAtSource(AudioSource src, AudioClip clip)
+    {
+        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
+        src.clip = clip;
+        //Play the clip.
+        src.PlayOneShot(src.clip);
+    }
+
+    public void PlaySingle(string clip)
+    {
+        var ac = GetAudioClip(clip);
+        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
+        efxSource.clip = ac;
+
+        //Play the clip.
+        efxSource.PlayOneShot(ac);
+    }
+
+    public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
+
+
+    public AudioClip GetAudioClip(string name)
+    {
+        return audioClips.Find(item => item.name.Substring(0) == name);
+    }
+
+    public void PlaySingleDelayed(AudioClip clip, float delay)
+    {
+        StartCoroutine(PlaySingleDelayedCoroutine(clip, delay));
+    }
+
+    public void PlaySingleDelayed(string clip, float delay)
+    {
+        StartCoroutine(PlaySingleDelayedCoroutine(GetAudioClip(clip), delay));
+    }
+
+    IEnumerator PlaySingleDelayedCoroutine(AudioClip clip, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        PlaySingle(clip);
+    }
+
+    //RandomizeSfx chooses randomly between various audio clips and slightly changes their pitch.
+    public void RandomizeSfx(params AudioClip[] clips)
+    {
+        //Generate a random number between 0 and the length of our array of clips passed in.
+        int randomIndex = Random.Range(0, clips.Length);
+
+        //Choose a random pitch to play back our clip at between our high and low pitch ranges.
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        //Set the pitch of the audio source to the randomly chosen pitch.
+        efxSource.pitch = randomPitch;
+
+        //Set the clip to the clip at our randomly chosen index.
+        efxSource.clip = clips[randomIndex];
+
+        //Play the clip.
+        efxSource.Play();
+    }
+}
--- homeward-ggj2019/Assets/MainMenu.cs
+++ homeward-ggj2019/Assets/MainMenu.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro; 
+
 public class MainMenu : MonoBehaviour
 {
+    public GameObject panelWithMusicAudioSource; 
+
     private void Start()
     {
-        
     }
     public void PlayGame()
     {
@@ -18,10 +21,30 @@ public class MainMenu : MonoBehaviour
     }
     public void ToggleMusic()
     {
-        //transform.Find("MUSIC OFF").GetComponent<TextMeshPro>()
+        if (PlayerPrefs.GetInt("MusicToggle") == 0)
+        {
+            GameObject.Find("MUSIC OFF").GetComponent<TextMeshProUGUI>().text = "MUSIC ON";
+            PlayerPrefs.SetInt("MusicToggle", 1);
+            panelWithMusicAudioSource.GetComponent<AudioSource>().enabled = true; 
+        }
+        else
+        {
+            GameObject.Find("MUSIC OFF").GetComponent<TextMeshProUGUI>().text = "MUSIC OFF";
+            PlayerPrefs.SetInt("MusicToggle", 0);
+            panelWithMusicAudioSource.GetComponent<AudioSource>().enabled = false;
+        }
     }
     public void ToggleFX()
     {
-
+        if (PlayerPrefs.GetInt("FXToggle") == 0)
+        {
+            GameObject.Find("SOUND FX OFF").GetComponent<TextMeshProUGUI>().text = "SOUND FX ON";
+            PlayerPrefs.SetInt("FXToggle", 1);
+        }
+        else
+        {
+            GameObject.Find("SOUND FX OFF").GetComponent<TextMeshProUGUI>().text = "SOUND FX OFF";
+            PlayerPrefs.SetInt("FXToggle", 0);
+        }
     }
 }
